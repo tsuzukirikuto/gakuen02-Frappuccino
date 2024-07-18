c23a035f32d09c95100a0a44bdfcceabc9e92008
都築陸音 <rikuto_tsuzuki@mail.toyota.co.jp>
2024-07-18 16:53:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ console.log(btn_02)
  
 function scrollMenu(){
    
-    window.scroll(0,400);
+    window.scroll(0,2050);
 }
  
 btn_02.addEventListener("click",scrollMenu);
+
+
+
+'use strict'
+// ここに JavaScript のコードを書きましょう
  
+const button = document.querySelector("#tyuumonn-button");
+console.log(button);
+ 
+const drink = document.querySelector("select");
+console.log(drink)
+
+
+
